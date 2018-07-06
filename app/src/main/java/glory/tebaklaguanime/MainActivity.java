dc3b0ad2f146ee763e1af9cae81d32aca555dc56
unknown <arifglory46@gmail.com>
2018-07-06 08:57:29 +0700

    rev6juli2018

--- app/src/main/java/glory/tebaklaguanime/MainActivity.java
+++ app/src/main/java/glory/tebaklaguanime/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +23,13 @@ import android.widget.TextView;
 import java.util.Currency;
 import java.util.List;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 import Kelas.DBAdapter;
 import Kelas.User;
@@ -31,14 +39,23 @@ public class MainActivity extends AppCompatActivity {
     Button btnPlay,btnExit;
     Intent i;
     DialogInterface.OnClickListener listener;
-    TextView txtCoin;
+    TextView txtCoin,txtFreeCoin;
     public static MediaPlayer clickSound;
-    ImageView imgAchievement,imgEvent,imgLeaderboard;
+    ImageView imgAchievement,imgEvent,imgLeaderboard,imgGift;
 
     DBAdapter mDB;
     public static User mUser;
     public static List<User> mlistUser;
     protected Cursor cursor;
+
+    private CountDownTimer mCountDownTimer;
+    private RewardedVideoAd mRewardedVideoAd;
+    private long mTimeRemaining;
+    private boolean mGameOver;
+    private boolean mGamePaused;
+    private static final long COUNTER_TIME = 10;
+    private static final int GAME_OVER_REWARD = 1;
+    private int kesempatanFreeCoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +70,14 @@ public class MainActivity extends AppCompatActivity {
         imgAchievement = (ImageView) findViewById(R.id.imgAchievement);
         imgEvent = (ImageView) findViewById(R.id.heart1);
         imgLeaderboard = (ImageView) findViewById(R.id.imgLeaderboard);
+        imgGift = (ImageView) findViewById(R.id.imgGift);
+        txtFreeCoin = (TextView) findViewById(R.id.txtFreeCoin);
 
         mDB = DBAdapter.getInstance(getApplicationContext());
         mlistUser = mDB.getDataUser();
         mUser = mlistUser.get(0);
         txtCoin.setText(String.valueOf(mUser.getCoin()));
+        kesempatanFreeCoin = mUser.getFree_coin();
 
 
 
@@ -106,6 +126,129 @@ public class MainActivity extends AppCompatActivity {
         TextView txtA = (TextView) findViewById(R.id.txtA);
         txtA.setTypeface(typeface);
 
+        //code buat ads Video
+        imgGift.setVisibility(View.INVISIBLE);
+        txtFreeCoin.setVisibility(View.INVISIBLE);
+        MobileAds.initialize(this, getString(R.string.myUnitID));
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(getApplicationContext());
+
+        mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
+            @Override
+            public void onRewardedVideoAdLoaded() {
+                Toast.makeText(getBaseContext(), "Iklan dimuat", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRewardedVideoAdOpened() {
+                Toast.makeText(getBaseContext(), "Iklan dibuka.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRewardedVideoStarted() {
+                Toast.makeText(getBaseContext(), "Iklan dimulai.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRewardedVideoAdClosed() {
+                Toast.makeText(getBaseContext(), "Iklan ditutup", Toast.LENGTH_SHORT).show();
+                muatUlangIklan();
+            }
+
+            @Override
+            public void onRewarded(RewardItem rewardItem) {
+                Toast.makeText(getBaseContext(), "Anda berhasil mendapatkan coin.", Toast.LENGTH_SHORT).show();
+                kesempatanFreeCoin = kesempatanFreeCoin - 1;
+                SQLiteDatabase db = mDB.getWritableDatabase();
+                db.execSQL("update tb_user set free_coin='"+kesempatanFreeCoin+"' where nama='User'");
+                mlistUser = mDB.getDataUser();
+                mUser = mlistUser.get(0);
+                kesempatanFreeCoin = mUser.getFree_coin();
+
+                muatUlangIklan();
+            }
+
+            @Override
+            public void onRewardedVideoAdLeftApplication() {
+                Toast.makeText(getBaseContext(), "Ad left application.", Toast.LENGTH_SHORT).show();
+                muatUlangIklan();
+            }
+
+            @Override
+            public void onRewardedVideoAdFailedToLoad(int i) {
+                Toast.makeText(getBaseContext(), "Ad failed to load.", Toast.LENGTH_SHORT).show();
+                muatUlangIklan();
+            }
+        });
+
+        if (kesempatanFreeCoin>0){
+            startGame();
+        }
+        imgGift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bunyiKlik();
+                showRewardedVideo();
+            }
+        });
+
+
+    }
+
+    private void muatUlangIklan(){
+        if (kesempatanFreeCoin>0){
+            startGame();
+        }
+    }
+
+    private void startGame() {
+        // Hide the retry button and start the timer.
+
+        imgGift.setVisibility(View.INVISIBLE);
+        txtFreeCoin.setVisibility(View.INVISIBLE);
+        createTimer(COUNTER_TIME);
+        mGamePaused = false;
+        mGameOver = false;
+
+        mRewardedVideoAd.loadAd(getString(R.string.tesUnitIDVideo), new AdRequest.Builder().build());
+    }
+
+    private void createTimer(long time) {
+
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+        mCountDownTimer = new CountDownTimer(time * 1000, 50) {
+            @Override
+            public void onTick(long millisUnitFinished) {
+                mTimeRemaining = ((millisUnitFinished / 1000) + 1);
+               // textView.setText("seconds remaining: " + mTimeRemaining);
+              //  Toast.makeText(getApplicationContext(),"Seconds remaining :"+mTimeRemaining,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFinish() {
+                gameOver();
+            }
+        };
+        mCountDownTimer.start();
+    }
+
+    private void gameOver() {
+
+        if (mRewardedVideoAd.isLoaded()) {
+            imgGift.setVisibility(View.VISIBLE);
+            txtFreeCoin.setVisibility(View.VISIBLE);
+        }
+
+        mGameOver = true;
+    }
+
+    private void showRewardedVideo(){
+        imgGift.setVisibility(View.INVISIBLE);
+        txtFreeCoin.setVisibility(View.INVISIBLE);
+        if (mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }
     }
 
     protected void OpenFacebookPage() {
